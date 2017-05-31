0589d574e36f45c7891c5727915402a9b1d07016
Alistair Legione <alegione@student.unimelb.edu.au>
2017-05-31 14:22:54 +1000

    Create Walltime.bash

---
+++ Walltime.bash
@@ -0,0 +1,25 @@
+#A function to include in bash scripts to detail walltime for loops
+#Get start time
+SCRIPTSTART=`date +%s`
+
+walltime () {
+	RED='\033[1;31m'
+	GREEN='\033[1;32m'
+	YELLOW='\033[1;33m'
+	BLUE='\033[1;34m'
+	PURPLE='\033[1;35m'
+	NOCOLOUR='\033[0m'	
+	TIMENOW=`date +%s`
+	STARTTIME=$1
+	TOTAL=$((TIMENOW-STARTTIME))
+	
+	if [ "$TOTAL" -lt "120" ]; then
+		echo -e "${BLUE}Total wall time: ${PURPLE}$TOTAL ${BLUE}seconds${NOCOLOUR}"
+	elif [ "$TOTAL" -lt "7200" ]; then
+		TOTAL=$(echo "$TOTAL/60" | bc)
+		echo -e "${BLUE}Total wall time: ${PURPLE}$TOTAL ${BLUE}minutes${NOCOLOUR}"
+	else
+		TOTAL=$(echo "$TOTAL/3600" | bc)
+		echo -e "${BLUE}Total wall time: ${PURPLE}$TOTAL ${BLUE}hours${NOCOLOUR}"
+	fi
+}
