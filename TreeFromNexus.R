ab5a990c6b4718dfa0807f3597fb4f7aefb2fe39
Alistair Legione <alegione@student.unimelb.edu.au>
2017-05-31 14:37:11 +1000

    Remove redundant lines

--- TreeFromNexus.R
+++ TreeFromNexus.R
@@ -1,11 +1,11 @@
-### To convert to Newick format
-## Made a tree conversion program in R, saved this as convert_tree.R
+### To convert MrBayes consensus tree output to Newick format
+### Inspired/adapted from code from Dr Neil Young
 #install.packages("ape")
 library(ape)
 args = commandArgs(trailingOnly=TRUE)
 options(digits=4)
 varSWD<-args[1]		#Current file directory
-varFileName<-args[2] #Current tree being converted
+varFileName<-args[2] #Current tree being converted (should end in *.con.tre)
 #varNexusInput<-paste0(varFileName,".con.tre")
 #varTreeOutput<-paste0(varFileName,".newick.tre")
 setwd(varSWD)
@@ -26,5 +26,3 @@ cat(varFileName,statMedian, statMin, statMax, varControl, sep = "\t")
 tree <- read.nexus(file=paste0(varFileName,".con.tre"))
 
 write.tree(tree,file=paste0(varFileName,".newick.tre"))
-# Loop through the Mr Bayes output trees and save with a new name. For this to work, you just have to make sure your trees end in ".nexus.mb.con.tre".
-#ls *nexus.mb.con.tre | sed 's/.nexus.mb.con.tre//1' | while read line; do cp $line.nexus.mb.con.tre in.tre; R --vanilla < convert_tree.R; mv temp.R.tre $line.mb.tre; done
