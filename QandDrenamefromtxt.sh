b73e1dca29f020cde83225dced94de78a288c5a7
Alistair Legione <alegione@student.unimelb.edu.au>
2017-07-31 18:42:51 +1000

    Delete QandDrenamefromtxt.sh

--- QandDrenamefromtxt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-mkdir "Shared_Folder/Cpec_whole_genome/ReferenceGenomes/renamed_draft_genomes"
-while read old new; do
-	cp "Shared_Folder/Cpec_whole_genome/ReferenceGenomes/draft_genomes/$old.fasta" "Shared_Folder/Cpec_whole_genome/ReferenceGenomes/renamed_draft_genomes/$new.fasta"
-done < renamefile.txt
